54815c8706ba7f899e31a1218fcab154661cc88f
Dorian Dessa <Dorian.dessa@fr.ey.com>
2023-11-19 15:18:13 +0100

    revu du code avec black et pylance

--- app_yt.py
+++ app_yt.py
@@ -1,25 +1,23 @@
 import streamlit as st
-from utils import Download
+from utils import download
 
-st.title('YouTube Downloader')
-url = st.text_input(label='Enter an url:')
+st.title("YouTube Downloader")
+url = st.text_input(label="Enter an url:")
 metadata = dict()
 
 if url:
-    folder_path = st.text_input(label='Enter the folder path in which you want the video to be downloaded:')
+    folder_path = st.text_input(
+        label="Enter the folder path in which you want the video to be downloaded:"
+    )
     if folder_path:
-        st.write("Downloading...")
-        metadata = Download(url, folder_path)
+        metadata = download(url, folder_path)
         st.success(f"Download of : {metadata['title']} is over ! 😀")
 
-        with open('log.txt', 'a') as f:
+        with open("log.txt", "a") as f:
             f.write(f"{metadata['time']}, Title : {metadata['title']} , {url}\n")
 
     else:
-        st.write('No folder path')
+        st.write("No folder path")
 
 
-#st.image('image/OIP.jpeg')
-
-
-
+# st.image('image/OIP.jpeg')
--- utils.py
+++ utils.py
@@ -1,28 +1,34 @@
 import os
+from datetime import datetime
 
 from pytube import YouTube
-from datetime import datetime
 import streamlit as st
 
 
-def Download(url, folder_path):
+def download(url, folder_path):
+    """
+    function that takes an url and a forder paths to download youtube videos online on local on your pc
+    return a dictionary object with title and date of downloading
+    """
+
     metadata = {}
+
+    st.write("Downloading...")
 
     yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
     yd = yt.streams.get_highest_resolution()
 
-    metadata = {'title': yd.title,
-                'time': datetime.now()
-                }
+    metadata = {"title": yd.title, "time": datetime.now()}
 
     if not os.path.isdir(folder_path):
-        st.error('The provided folder does not exist. Please provide a valid folder path.')
+        st.error(
+            "The provided folder does not exist. Please provide a valid folder path."
+        )
         return
 
-    file_path = os.path.join(folder_path, metadata['title'])
+    file_path = os.path.join(folder_path, metadata["title"])
 
     # Download video to in the appropriate folder
     yd.download(file_path)
 
     return metadata
-
